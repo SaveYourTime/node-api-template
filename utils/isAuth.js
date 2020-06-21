c3f7619c01c974e1d82e11fbd8eabd11e4a5537c
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-22 00:26:24 +0800

    Fixed uncatched error: jwt must be provided

--- utils/isAuth.js
+++ utils/isAuth.js
@@ -3,7 +3,12 @@ import users from '../data/users';
 
 const isAuth = (req, res, next) => {
   const { token } = req.cookies;
-  if (!token) next(new Error('Token is not found!'));
+  if (!token) {
+    const err = new Error();
+    err.statusCode = 401;
+    err.message = 'Token is not found!';
+    return next(err);
+  }
   const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
   const user = users.find(({ id }) => id === payload.id);
   if (user) {
